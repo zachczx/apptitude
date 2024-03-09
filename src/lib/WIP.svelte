b71b5a12eb4fe70e377e92353cd4effb5302c40b
zachczx <zixian.chen@gmail.com>
2024-03-10 01:40:45 +0800

    Update WIP.svelte

--- src/lib/WIP.svelte
+++ src/lib/WIP.svelte
@@ -4,5 +4,9 @@
 
 <div class="grid justify-items-center gap-y-5">
 	<div class="mb-5"><h1 class="pb-6">Work in progress</h1></div>
-	<div><img src={wip} alt="Work in progress" class="" /></div>
+	<div class="avatar">
+		<div class="w-full mask mask-squircle">
+			<img src={wip} alt="Work in progress" class="" />
+		</div>
+	  </div>
 </div>
