3befb49993eaf475f8ed1c6ce18deb0cacc0a7a7
zachczx <zixian.chen@gmail.com>
2024-03-16 10:40:32 +0800

    added techtrippin page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,8 +14,8 @@
 					<p class="mb-5"><span class="text-primary font-bold">Apptitude</span> helps you learn new competencies and what good tech decisions look like, so you can improve outcomes for your tech products and projects.</p>
 					<p class="mb-5">Stop the buck where it's at. Your users and customers deserve good products at the lowest cost. </p>
 					<p class="mb-5">Right the wrongs of tech in the public sector. The slow, laggy, bloated apps. The overspending. The timeline delays.</p>
-					<p class="mt-10 mb-5 space-x-2 lg:space-x-5"><a href="/skills" class="btn btn-primary lg:text-xl">Start Learning</a>
-					<a href="/cacheup" class="btn btn-primary btn-outline lg:text-xl">Quickstart Guides</a></p>
+					<p class="mt-10 mb-5 space-x-2 lg:space-x-5"><a href="/skills" class="btn bg-lime-600 hover:bg-lime-800 text-white lg:text-xl font-medium">Start Learning <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right inline align-top"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg></a>
+					<a href="/cacheup" class="btn btn-primary text-lime-600 hover:bg-lime-600 btn-outline lg:text-xl font-medium">Quickstart Guides</a></p>
 				</div>
 	<div class="avatar flex lg:w-2/3 px-12 lg:col-span-2 mt-5">
 		<div class="rounded-full">
--- src/routes/techtrippin/+page.svelte
+++ src/routes/techtrippin/+page.svelte
@@ -1,20 +1,49 @@
 <script>
 	import Wip from "$lib/WIP.svelte";
+	import { techtrippin } from "./data_techtrippin";
+	import consultant from "$lib/assets/consultant.webp?enhanced&w=200;150;70";
+	import yesChad from "$lib/assets/yeschad.webp?enhanced&w=200;150;70";
 </script>
 
-<!--
+
 <div class="mb-10 text-center lg:mb-20">
 	<h1
 		class="inline-block bg-gradient-to-r from-emerald-200 via-orange-200 to-pink-200 bg-clip-text py-3 text-transparent"
 	>
 		Tech Trippin'
 	</h1>
-	<div><i>Find your own path upsizing your Bytes into Megabytes.</i></div>
+	<div><i>Decrypting garbled tech jargon-gibberish (wannabe) "tech" consultants, designed to use many words to say absolutely nothing.</i></div>
 </div>
 <div
-	class="grid grid-cols-1 justify-items-center gap-x-8 gap-y-8 px-5 lg:grid-cols-2 lg:px-20 xl:grid-cols-4"
+	class="grid grid-cols-1 justify-items-center gap-x-8 gap-y-8 px-5 lg:grid-cols-2 lg:px-40 xl:grid-cols-1"
 >
+{#each techtrippin as techtrippinItem}
 
-</div>-->
 
-<Wip />	
+<div>
+<div class="chat chat-start">
+	<div class="chat-image avatar">
+	  <div class="w-10 lg:w-[200px] rounded-full">
+		<enhanced:img alt="Professional Tech Consultant" src={consultant} 
+		sizes="(min-width:1920px) 200px, (min-width:1080px) 150px, (min-width:768px) 70px"
+		/>
+	  </div>
+	</div>
+	<div class="chat-header">
+	</div>
+	<div class="chat-bubble">{techtrippinItem.prompt}</div>
+  </div>
+  <div class="chat chat-end">
+	<div class="chat-image avatar">
+		<div class="w-10 lg:w-[200px] rounded-full">
+		<enhanced:img alt="Chad" src={yesChad} 
+		sizes="(min-width:1920px) 200px, (min-width:1080px) 150px, (min-width:768px) 70px"
+		/>	  </div>
+	</div>
+	<div class="chat-header">
+	</div>
+	<div class="chat-bubble">{techtrippinItem.response}</div>
+  </div>
+</div>
+{/each}
+</div>
