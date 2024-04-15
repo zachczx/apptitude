3e39b581b2e904dc93b74c2877eec5e52b0e2b3e
zachczx <zixian.chen@gmail.com>
2024-04-15 23:06:01 +0800

    Update HowThisWorks.svelte

--- src/lib/HowThisWorks.svelte
+++ src/lib/HowThisWorks.svelte
@@ -1,19 +1,19 @@
 <script>
 	import pandaScientist800 from '$lib/assets/panda-scientist-800.webp';
 	import pandaSuperhero from '$lib/assets/panda-superhero.webp?enhanced&w=400';
-	import pandaBirds from '$lib/assets/panda-birds.webp?enhanced&w=400';
+	import pandaBirds from '$lib/assets/panda-birds.webp?enhanced&w=600';
 	import pandaNo from '$lib/assets/panda-no.webp?enhanced&w=400';
 	import pandaReading2 from '$lib/assets/panda-reading2.webp?enhanced&w=500';
 	import pandaLabGoggles from '$lib/assets/panda-lab-goggles.webp?enhanced&w=200';
 	import swoleDoge from '$lib/assets/swole-doge.webp?enhanced&w=200';
 </script>
 
-<div class="bg-gray-900 px-3 lg:px-10 lg:py-20">
+<div class="bg-gray-900 px-3 py-10 lg:px-10 lg:py-20">
 	<h1
 		class="pb-20 pt-10 text-center text-5xl text-lime-400 lg:pt-5 lg:text-6xl xl:text-7xl min-[1921px]:text-8xl">
 		How Apptitude Helps You
 	</h1>
-	<div class="grid gap-x-4 gap-y-4 lg:grid-cols-7">
+	<div class="grid gap-x-5 gap-y-10 lg:grid-cols-7">
 		<div
 			class="card col-span-3 border border-gray-700 bg-base-100 shadow-md shadow-gray-950 transition duration-500 ease-in-out hover:bg-gray-700 lg:col-span-1 lg:row-span-1">
 			<div class="card-body grid h-48 content-center">
@@ -37,7 +37,7 @@
 		</div>
 
 		<div
-			class="card bg-base-100 p-5 lg:col-span-2 lg:row-span-2"
+			class="card col-span-3 h-96 bg-base-100 p-5 lg:col-span-2 lg:row-span-2 lg:h-full"
 			style="background-image:url({pandaScientist800}); background-size: cover; background-position:top center;">
 		</div>
 		<div
