0b166e985d4e5655e693953dd981ebdb075c089a
zachczx <zixian.chen@gmail.com>
2024-06-25 22:01:54 +0800

    -

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
+import pagefind from 'vite-plugin-pagefind';
 
 export default defineConfig({
 	plugins: [enhancedImages(), sveltekit()],
+	/* 	, pagefind() build: {
+		rollupOptions: {
+			external: ['/pagefind/pagefind.js', '/pagefind/pagefind-ui.js'],
+		},
+	},
+	assetsInclude: 'pagefind.js', */
 });
