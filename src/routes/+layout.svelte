1c5cb3bcd6c3c754e1aff41e27a77805d54737d4
zachczx <zixian.chen@gmail.com>
2024-06-27 00:02:09 +0800

    pagefind ui tweaks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -104,6 +104,23 @@
 			</li>
 		</ul>
 	</div>
+	<div class="navbar-end">
+		<a href="/search"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="2em"
+				height="2em"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="icon icon-tabler icons-tabler-outline icon-tabler-search me-3 inline"
+				><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+					d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
+		</a>
+	</div>
 </div>
 
 <!-- {#if !viewTransition}
