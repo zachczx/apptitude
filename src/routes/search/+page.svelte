51d915bbdf33d5a894ed87902e8da8122e5b00cb
zachczx <zixian.chen@gmail.com>
2024-06-27 15:33:15 +0800

    Update +page.svelte

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,10 +1,5 @@
 <script>
 	import { onMount } from 'svelte';
-
-	let { data } = $props();
-	$effect(() => {
-		console.log(data);
-	});
 	onMount(async () => {
 		// @ts-expect-error - Pagefind will be present at runtime
 		const pagefind = await import('/pagefind/pagefind.js');
