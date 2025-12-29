7a10d54c1eaad4b4a2684246952a9f0c722b9d78
zachczx <zixian.chen@gmail.com>
2025-12-30 01:12:34 +0800

    feat: make left scrollbar thinner

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -7,7 +7,7 @@
 
 <div class="grid grid-cols-[auto_1fr_auto]">
 	<aside
-		class="lg:border-r-base-100 vertical-offset sticky top-[56px] hidden w-full min-w-72 content-start overflow-y-auto lg:grid lg:border-r-2">
+		class="lg:border-r-base-100 vertical-offset sidebar-color sticky top-14 hidden w-full min-w-72 content-start overflow-y-auto lg:grid lg:border-r-2">
 		<NavAside {page} {category} />
 	</aside>
 	<main class="border-r-base-100 bg-base-100 w-full border-r-2">
@@ -18,5 +18,24 @@
 <style>
 	.vertical-offset {
 		height: calc(100dvh - 56px);
+	}
+
+	.sidebar-color {
+		scrollbar-width: thin;
+		scrollbar-color: #444 transparent;
+	}
+
+	.sidebar-color::-webkit-scrollbar {
+		width: 8px;
+	}
+	.sidebar-color::-webkit-scrollbar-track {
+		background: #0d0d0d;
+	}
+	.sidebar-color::-webkit-scrollbar-thumb {
+		background: #333;
+		border-radius: 4px;
+	}
+	.sidebar-color::-webkit-scrollbar-thumb:hover {
+		background: #555;
 	}
 </style>
